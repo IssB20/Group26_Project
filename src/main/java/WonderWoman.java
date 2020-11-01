99ccfaee7acc19e59ef8d71b1949856ced4f854e
lukykatie <lukykatie@gmail.com>
2020-11-01 16:25:51 -0500

    changes

--- src/main/java/WonderWoman.java
+++ src/main/java/WonderWoman.java
@@ -1,2 +1,8 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class WonderWoman {
+
+    public static void main(String[] args) {
+        System.out.println("new changes");
+    }
 }
